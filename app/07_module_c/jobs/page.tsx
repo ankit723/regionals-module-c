85891a7690264ba4de736683df853fec756d2ed8
Ankit Biswas <varanasiartist.omg@gmail.com>
2026-03-01 12:35:54 +0530

    fixed build err

---
+++ app/07_module_c/jobs/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { Button } from "@/components/ui/button"
+import {data} from "@/data/data"
+import { ComputerIcon } from "lucide-react"
+import Link from "next/link"
+import { useParams } from "next/navigation"
+import { toast } from "sonner"
+
+export default function SectorsPage(){
+    
+
+
+    return(
+        <div className="">
+            <div className="mb-7 -mt-5">
+                <h1 className="text-2xl font-bold">Job Desription</h1>
+                <p className="text-slate-600 font-medium">Here is your requested job descriptiopn</p>
+            </div>
+    
+        </div>
+    )
+}
